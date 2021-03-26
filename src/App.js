fd538a325ca2e1af9f839dfda24b0be38fe2d3f5
kristinadurant <kristinadurant.p@gmail.com>
2021-03-26 12:03:57 -0400

    installed packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component="Home" />
+        <Route exact path='/login' component="Login" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
