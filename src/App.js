0e0801b097115ab8d43854eeef2af85a80eb6a76
kristinadurant <kristinadurant.p@gmail.com>
2021-03-26 12:23:02 -0400

    aredux setup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from "./redux/store";
+import { Home, Login } from './components';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component="Home" />
-        <Route exact path='/login' component="Login" />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
