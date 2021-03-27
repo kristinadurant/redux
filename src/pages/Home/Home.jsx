26a02d89ef1dceb601d550705ddb875b4222f151
kristinadurant <kristinadurant.p@gmail.com>
2021-03-27 12:42:52 -0400

    connected home to the store

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
-const Home = () => {
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Home = ({ email }) => {
     return (
-        <div>
-            Home
-        </div>
+        <>
+            <h1>Welcome {email}</h1>
+            <Link to='/login'>Login</Link>
+        </>
     )
 }
 
-export default Home
+const mapStateToProps = state => ({ email: state.auth.email });
+
+export default connect(mapStateToProps)(Home);
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { emailChanged, passwordChanged } from '../../redux/actions';
 import { Input } from '../../components';
 
 const Login = (props) => {
-    console.log(props.email)
     return (
-        <form>
-            <h1>Login</h1>
-            <Input 
-                type='email' id='email' label='Email' placeholder='Email' required 
-                onChange={e => props.emailChanged(e.target.value)}
-                value={props.email}
-            />
-            <Input 
-                type='password' id='password' label='Password' placeholder='Password' required 
-                onChange={e => props.passwordChanged(e.target.value)}
-                value={props.pass}
-            />
-            <button type='submit'>Login</button>
-        </form>
+        <>
+            <Link to='/'>Back</Link>
+            <form>
+                <h1>Login</h1>
+                <Input 
+                    type='email' id='email' label='Email' placeholder='Email' required 
+                    onChange={e => props.emailChanged(e.target.value)}
+                    value={props.email}
+                />
+                <Input 
+                    type='password' id='password' label='Password' placeholder='Password' required 
+                    onChange={e => props.passwordChanged(e.target.value)}
+                    value={props.pass}
+                />
+                <button type='submit'>Login</button>
+            </form>
+        </>
     )
 }
 
